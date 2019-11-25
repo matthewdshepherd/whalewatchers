7384e42554ebaf031c3ce297f39fc1727dadebe7
Matthew Malone <m.malone@me.com>
2019-11-25 00:31:44 -0500

    Update the heroku link on the info section

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
                 <div className="api-bottom-seperator__div"></div>
               </div>
               <div className="api-block__section__div">
-                <h4 className="api-link__h4">www.heroku.com <span className="api-link__span">/api/v1/beaches</span></h4>
+                <h4 className="api-link__h4">https://byob-whalewatchers.herokuapp.com<span className="api-link__span">/api/v1/beaches</span></h4>
                 <div className="fetch__div">
                   <p className="fetch--request__p">GET</p>
                   <p className="fetch--request--return__p">Returns all beaches in JSON format</p>
@@ -35,7 +35,7 @@ function App() {
                 <div className="api-bottom-seperator__div"></div>
               </div>
               <div className="api-block__section__div">
-                <h4 className="api-link__h4">www.heroku.com <span className="api-link__span">v1/beaches/:id</span></h4>
+                <h4 className="api-link__h4">https://byob-whalewatchers.herokuapp.com<span className="api-link__span">v1/beaches/:id</span></h4>
                 <div className="fetch__div">
                   <p className="fetch--request__p">GET</p>
                   <p className="fetch--request--return__p">Returns a specific beach sightings in JSON format</p>
@@ -43,7 +43,7 @@ function App() {
                 <div className="api-bottom-seperator__div"></div>
               </div>
               <div className="api-block__section__div">
-                <h4 className="api-link__h4">www.heroku.com <span className="api-link__span">/api/v1/whale_sightings</span></h4>
+                <h4 className="api-link__h4">https://byob-whalewatchers.herokuapp.com<span className="api-link__span">/api/v1/whale_sightings</span></h4>
                 <div className="fetch__div">
                   <p className="fetch--request__p">GET</p>
                   <p className="fetch--request--return__p">Returns all whale sightings in JSON format</p>
@@ -51,7 +51,7 @@ function App() {
                 <div className="api-bottom-seperator__div"></div>
               </div>
               <div className="api-block__section__div">
-                <h4 className="api-link__h4">www.heroku.com <span className="api-link__span">/api/v1/beaches/:id/whale_sightings</span></h4>
+                <h4 className="api-link__h4">https://byob-whalewatchers.herokuapp.com<span className="api-link__span">/api/v1/beaches/:id/whale_sightings</span></h4>
                 <div className="fetch__div">
                   <p className="fetch--request__p">GET</p>
                   <p className="fetch--request--return__p">Returns 100 most recent whale sightings for specific beach in JSON format</p>
@@ -59,7 +59,7 @@ function App() {
                 <div className="api-bottom-seperator__div"></div>
               </div>
               <div className="api-block__section__div">
-                <h4 className="api-link__h4">www.heroku.com <span className="api-link__span">/api/v1/beaches/sighting_type/:id</span></h4>
+                <h4 className="api-link__h4">https://byob-whalewatchers.herokuapp.com<span className="api-link__span">/api/v1/beaches/sighting_type/:id</span></h4>
                 <div className="fetch__div">
                   <p className="fetch--request__p">GET</p>
                   <p className="fetch--request--return__p">Returns all beaches where there was a whale/marine animal sighting of the requested type in JSON format</p>
@@ -67,7 +67,7 @@ function App() {
                 <div className="api-bottom-seperator__div"></div>
               </div>
               <div className="api-block__section__div">
-                <h4 className="api-link__h4">www.heroku.com <span className="api-link__span">/api/v1/beaches</span></h4>
+                <h4 className="api-link__h4">https://byob-whalewatchers.herokuapp.com<span className="api-link__span">/api/v1/beaches</span></h4>
                 <div className="fetch__div">
                   <p className="fetch--request__p">POST</p>
                   <p className="fetch--request--return__p">Creates a new beach, must in JSON format</p>
@@ -75,7 +75,7 @@ function App() {
                 <div className="api-bottom-seperator__div"></div>
               </div>
               <div className="api-block__section__div">
-                <h4 className="api-link__h4">www.heroku.com <span className="api-link__span">/api/v1/whale_sightings</span></h4>
+                <h4 className="api-link__h4">https://byob-whalewatchers.herokuapp.com<span className="api-link__span">/api/v1/whale_sightings</span></h4>
                 <div className="fetch__div">
                   <p className="fetch--request__p">POST</p>
                   <p className="fetch--request--return__p">Creates a new whale sighting, must send in JSON format</p>
@@ -83,7 +83,7 @@ function App() {
                 <div className="api-bottom-seperator__div"></div>
               </div>
               <div className="api-block__section__div">
-                <h4 className="api-link__h4">www.heroku.com <span className="api-link__span">/api/v1/whale_sightings/:id</span></h4>
+                <h4 className="api-link__h4">https://byob-whalewatchers.herokuapp.com<span className="api-link__span">/api/v1/whale_sightings/:id</span></h4>
                 <div className="fetch__div">
                   <p className="fetch--request__p">DELETE</p>
                   <p className="fetch--request--return__p">Deletes whale sighting</p>
