3b476d6f38b39e1da08d749c9b482d33789ce06a
Matthew Malone <m.malone@me.com>
2019-11-25 00:14:23 -0500

    Updates

---
+++ precache-manifest.a10039056989ba5c94bdb1df17d37456.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "913489edc26fc0dfe3b2978742c27bb4",
+    "url": "/whalewatchers/index.html"
+  },
+  {
+    "revision": "e12b4674b8a786466bc5",
+    "url": "/whalewatchers/static/css/main.7118c53a.chunk.css"
+  },
+  {
+    "revision": "d1a9c9a7595c1a2e7823",
+    "url": "/whalewatchers/static/js/2.18204bcc.chunk.js"
+  },
+  {
+    "revision": "e12b4674b8a786466bc5",
+    "url": "/whalewatchers/static/js/main.63969aa9.chunk.js"
+  },
+  {
+    "revision": "34f44a00916740974d80",
+    "url": "/whalewatchers/static/js/runtime-main.82d644b7.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/whalewatchers/static/media/logo.25bf045c.svg"
+  }
+]);
